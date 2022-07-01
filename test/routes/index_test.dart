8ee1f4af39e47f6ca664e719e5ef82dbd5c512bc
ryota0624 <tennis.suta6@gmail.com>
2022-07-01 17:46:24 +0900

    fix test

--- test/routes/index_test.dart
+++ test/routes/index_test.dart
@@ -12,11 +12,13 @@ void main() {
   group('GET /', () {
     test('responds with a 200 and "Welcome to Dart Frog!".', () {
       final context = _MockRequestContext();
+      const provided = "provided";
+      when(() => context.read<String>()).thenReturn(provided);
       final response = route.onRequest(context);
       expect(response.statusCode, equals(HttpStatus.ok));
       expect(
         response.body(),
-        completion(equals('Welcome to Dart Frog!')),
+        completion(equals(provided)),
       );
     });
   });
